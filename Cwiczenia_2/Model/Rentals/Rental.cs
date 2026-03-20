2a9a94edab774898ad52e74117cc9a6895a87632
Daniel Mączak <s29832@pjwstk.edu.pl>
2026-03-20 22:00:36 +0100

    Dodanie klasy wypozyczenia

---
+++ Cwiczenia_2/Model/Rentals/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cwiczenia_2.Model.Users;
+using Cwiczenia_2.Model.Equipments;
+
+namespace Cwiczenia_2.Model.Rentals;
+
+public class Rental
+{
+    public User User { get; private set; }
+    public Equipment Equipment { get; private set; }
+    public DateTime RentalDate { get; private set; }
+    public DateTime ReturnDatePlanned { get; private set; }
+    public DateTime? ReturnDateReally { get; private set; }
+    public int? Punishment { get; private set; }
+
+    public Rental(User user, Equipment equipment, int days)
+    {
+        User = user;
+        Equipment = equipment;
+        RentalDate = DateTime.Now;
+        ReturnDatePlanned = RentalDate.AddDays(days);
+    }
+}
