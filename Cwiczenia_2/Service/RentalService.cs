9fc49ec4809b9de5b39e8fea96b97061c430f8b8
Daniel Mączak <s29832@pjwstk.edu.pl>
2026-03-21 14:59:36 +0100

    Dodanie metody Rent i Return do RentalService

--- Cwiczenia_2/Service/RentalService.cs
+++ Cwiczenia_2/Service/RentalService.cs
@@ -1,14 +1,59 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Cwiczenia_2.Model.Equipments;
 using Cwiczenia_2.Model.Rentals;
+using Cwiczenia_2.Model.Users;
 
 namespace Cwiczenia_2.Service;
 
 public class RentalService
 {
-    private List<Rental> rentals;
+    private readonly List<Rental> rentals = new();
 
-    public RentalService()
+    public void Rent(User user, Equipment equipment, int days)
     {
-        rentals = new List<Rental>();
+        if (!equipment.IsAvailable)
+        {
+            Console.WriteLine($" {equipment.Name} is not available!");
+            return;
+        }
+        int activeRent = rentals.Count(rental => rental.User == user && rental.ReturnDateReally == null);
+        
+        if (activeRent >= user.MaxLimit)
+        {
+            Console.WriteLine($"{user.Name} has reached the limit of rentals!");
+            return; 
+        }
+
+        var newRental = new Rental(user, equipment, days);
+        rentals.Add(newRental);
+        equipment.setUnavailable();
+        Console.WriteLine("Equipment rented!");
+        
+    }
+
+    public void Return(User user, Equipment equipment, DateTime returnDate)
+    {
+        var rental = rentals.FirstOrDefault(r => 
+            r.User == user && 
+            r.Equipment == equipment && 
+            r.ReturnDateReally == null);
+        if (rental == null)
+        {
+            Console.WriteLine("Error: Equipment already returned or not rented by this user!");
+            return;
+        }
+        int punishment = 0;
+    
+        if (returnDate > rental.ReturnDatePlanned)
+        {
+            int daysLate = (int) (returnDate - rental.ReturnDatePlanned).TotalDays;
+            punishment = daysLate * 20;
+            Console.WriteLine($"Punishment: {punishment} zł");
+        }
+        rental.Equipment.setAvailable();
+        rental.ReturnRegister(returnDate, punishment);
+    
+        Console.WriteLine("Equipment returned!");
     }
 }
